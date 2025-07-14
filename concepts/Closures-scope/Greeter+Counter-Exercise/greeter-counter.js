9c20a75a82dbb5401dbb106914506146078a1c3b
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2025-07-14 00:35:01 -0500

    Feat: 💬 Add greeting generator with closure to practice scope and function factories

--- concepts/Closures-scope/Greeter+Counter-Exercise/greeter-counter.js
+++ concepts/Closures-scope/Greeter+Counter-Exercise/greeter-counter.js
@@ -4,13 +4,15 @@ const createBtn = document.getElementById("createBtn");
 
 const makeGreeter = (name) => {
   return (message) => {
-    return `Welcome: ${name} ${message}`
+    return ` ${name} ${message}`;
   };
 };
-createBtn.addEventListener('click', ()=>{
-const greetingOutput = document.getElementById('greeterList')
-const input = nameInput.value
-makeGreeter(input)
-const greeting = makeGreeter("Nice to have you!")
-greetingOutput.textContent 
-})
+createBtn.addEventListener("click", () => {
+  const greetingOutput = document.getElementById("greeterList");
+  const input = nameInput.value;
+  nameInput.focus();
+  const greetUser = makeGreeter(input);
+  const greeting = greetUser("Nice to have you!");
+  greetingOutput.textContent += `Welcome: ${greeting}`;
+  console.log(greetUser);
+});
